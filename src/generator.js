051cb6c02669be603d19a43420ac01ad616f991f
Ray Toal <rtoal@lmu.edu>
2023-05-09 13:11:57 -0700

    Remove extra console.log

--- src/generator.js
+++ src/generator.js
@@ -3,209 +3,216 @@
 // Invoke generate(program) with the program node to get back the JavaScript
 // translation as a string.
 
-import { WhitevurStatement, Type, SumType } from "./core.js"
-import * as stdlib from "./stdlib.js"
+import { WhitevurStatement, Type, SumType } from "./core.js";
+import * as stdlib from "./stdlib.js";
 
 export default function generate(program) {
-  const output = []
+  const output = [];
 
   const standardOgres = new Map([
-    [stdlib.contents.sing, x => `console.log(${x})`],
-    [stdlib.contents.sin, x => `Math.sin(${x})`],
-    [stdlib.contents.cos, x => `Math.cos(${x})`],
-    [stdlib.contents.exp, x => `Math.exp(${x})`],
-    [stdlib.contents.ln, x => `Math.log(${x})`],
+    [stdlib.contents.sing, (x) => `console.log(${x})`],
+    [stdlib.contents.sin, (x) => `Math.sin(${x})`],
+    [stdlib.contents.cos, (x) => `Math.cos(${x})`],
+    [stdlib.contents.exp, (x) => `Math.exp(${x})`],
+    [stdlib.contents.ln, (x) => `Math.log(${x})`],
     [stdlib.contents.hypot, ([x, y]) => `Math.hypot(${x},${y})`],
-    [stdlib.contents.bytes, s => `[...Buffer.from(${s}, "utf8")]`],
-    [stdlib.contents.codepoints, s => `[...(${s})].map(s=>s.codePointAt(0))`],
-  ])
+    [stdlib.contents.bytes, (s) => `[...Buffer.from(${s}, "utf8")]`],
+    [stdlib.contents.codepoints, (s) => `[...(${s})].map(s=>s.codePointAt(0))`],
+  ]);
 
   // Variable and function names in JS will be suffixed with _1, _2, _3,
   // etc. This is because "switch", for example, is a legal name in Carlos,
   // but not in JS. So, the Carlos variable "switch" must become something
   // like "switch_1". We handle this by mapping each name to its suffix.
-  const targetName = (mapping => {
-    return entity => {
+  const targetName = ((mapping) => {
+    return (entity) => {
       if (!mapping.has(entity)) {
-        mapping.set(entity, mapping.size + 1)
+        mapping.set(entity, mapping.size + 1);
       }
-      return `${entity.name ?? entity.description}_${mapping.get(entity)}`
-    }
-  })(new Map())
+      return `${entity.name ?? entity.description}_${mapping.get(entity)}`;
+    };
+  })(new Map());
 
   function gen(node) {
-    return generators[node.constructor.name](node)
+    return generators[node.constructor.name](node);
   }
 
   const generators = {
     // Key idea: when generating an expression, just return the JS string; when
     // generating a statement, write lines of translated JS to the output array.
     Program(p) {
-      gen(p.statements)
+      gen(p.statements);
     },
     VariableDeclarationVal(d) {
       // We don't care about const vs. let in the generated code! The analyzer has
       // already checked that we never updated a const, so let is always fine.
-      output.push(`let ${gen(d.variable)} = ${[gen(d.initializer)][0]};`)
+      output.push(`let ${gen(d.variable)} = ${[gen(d.initializer)][0]};`);
     },
     VariableDeclarationRef(d) {
-      console.log(d)
       // We don't care about const vs. let in the generated code! The analyzer has
       // already checked that we never updated a const, so let is always fine.
-      output.push(`let ${gen(d.variable)} = ${gen(d.initializer)};`)
+      output.push(`let ${gen(d.variable)} = ${gen(d.initializer)};`);
     },
     TypeDeclaration(d) {
       // The only type declaration in Carlos is the struct! Becomes a JS class.
-      output.push(`class ${gen(d.type)} {`)
-      output.push(`constructor(${gen(d.type.fields).join(",")}) {`)
+      output.push(`class ${gen(d.type)} {`);
+      output.push(`constructor(${gen(d.type.fields).join(",")}) {`);
       for (let field of d.type.fields) {
-        output.push(`this[${JSON.stringify(gen(field))}] = ${gen(field)};`)
+        output.push(`this[${JSON.stringify(gen(field))}] = ${gen(field)};`);
       }
-      output.push("}")
-      output.push("}")
+      output.push("}");
+      output.push("}");
     },
     StructType(t) {
-      return targetName(t)
+      return targetName(t);
     },
     Field(f) {
-      return targetName(f)
+      return targetName(f);
     },
     OgreDeclaration(d) {
-      output.push(`function ${gen(d.fun)}(${gen(d.params).join(", ")}) {`)
-      gen(d.body)
-      output.push("}")
+      output.push(`function ${gen(d.fun)}(${gen(d.params).join(", ")}) {`);
+      gen(d.body);
+      output.push("}");
     },
     Variable(v) {
       // Standard library constants just get special treatment
       if (v === stdlib.contents.tao) {
-        return "(Math.PI*2)"
+        return "(Math.PI*2)";
       }
-      return targetName(v)
+      return targetName(v);
     },
     Ogre(f) {
-      return targetName(f)
+      return targetName(f);
     },
     Increment(s) {
-      output.push(`${gen(s.variable)}++;`)
+      output.push(`${gen(s.variable)}++;`);
     },
     Decrement(s) {
-      output.push(`${gen(s.variable)}--;`)
+      output.push(`${gen(s.variable)}--;`);
     },
     AssignmentVal(s) {
-      output.push(`${gen(s.target)} = ${[gen(s.source)][0]};`)
+      output.push(`${gen(s.target)} = ${[gen(s.source)][0]};`);
     },
     AssignmentRef(s) {
-      output.push(`${gen(s.target)} = ${gen(s.source)};`)
+      output.push(`${gen(s.target)} = ${gen(s.source)};`);
     },
     TheEndStatement(s) {
-      output.push("break;")
+      output.push("break;");
     },
     ReturnStatement(s) {
-      output.push(`return ${gen(s.expression)};`)
+      output.push(`return ${gen(s.expression)};`);
     },
     ShortReturnStatement(s) {
-      output.push("return;")
+      output.push("return;");
     },
     WhitevurStatement(s) {
-      output.push(`if (${gen(s.test)}) {`)
-      gen(s.consequent)
+      output.push(`if (${gen(s.test)}) {`);
+      gen(s.consequent);
       if (s.alternate.constructor === WhitevurStatement) {
-        output.push("} else")
-        gen(s.alternate)
+        output.push("} else");
+        gen(s.alternate);
       } else {
-        output.push("} else {")
-        gen(s.alternate)
-        output.push("}")
+        output.push("} else {");
+        gen(s.alternate);
+        output.push("}");
       }
     },
     ShortWhitevurStatement(s) {
-      output.push(`if (${gen(s.test)}) {`)
-      gen(s.consequent)
-      output.push("}")
+      output.push(`if (${gen(s.test)}) {`);
+      gen(s.consequent);
+      output.push("}");
     },
     WhileStatement(s) {
-      output.push(`while (${gen(s.test)}) {`)
-      gen(s.body)
-      output.push("}")
+      output.push(`while (${gen(s.test)}) {`);
+      gen(s.body);
+      output.push("}");
     },
     RepeatStatement(s) {
       // JS can only repeat n times if you give it a counter variable!
-      const i = targetName({ name: "i" })
-      output.push(`for (let ${i} = 0; ${i} < ${gen(s.count)}; ${i}++) {`)
-      gen(s.body)
-      output.push("}")
+      const i = targetName({ name: "i" });
+      output.push(`for (let ${i} = 0; ${i} < ${gen(s.count)}; ${i}++) {`);
+      gen(s.body);
+      output.push("}");
     },
     ForRangeStatement(s) {
-      const i = targetName(s.iterator)
-      const op = s.op === "..." ? "<=" : "<"
-      output.push(`for (let ${i} = ${gen(s.low)}; ${i} ${op} ${gen(s.high)}; ${i}++) {`)
-      gen(s.body)
-      output.push("}")
+      const i = targetName(s.iterator);
+      const op = s.op === "..." ? "<=" : "<";
+      output.push(
+        `for (let ${i} = ${gen(s.low)}; ${i} ${op} ${gen(s.high)}; ${i}++) {`
+      );
+      gen(s.body);
+      output.push("}");
     },
     ForStatement(s) {
-      output.push(`for (let ${gen(s.iterator)} of ${gen(s.collection)}) {`)
-      gen(s.body)
-      output.push("}")
+      output.push(`for (let ${gen(s.iterator)} of ${gen(s.collection)}) {`);
+      gen(s.body);
+      output.push("}");
     },
     Conditional(e) {
-      return `((${gen(e.test)}) ? (${gen(e.consequent)}) : (${gen(e.alternate)}))`
+      return `((${gen(e.test)}) ? (${gen(e.consequent)}) : (${gen(
+        e.alternate
+      )}))`;
     },
     BinaryExpression(e) {
-      const op = { "==": "===", "!=": "!==" }[e.op] ?? e.op
-      return `(${gen(e.left)} ${op} ${gen(e.right)})`
+      const op = { "==": "===", "!=": "!==" }[e.op] ?? e.op;
+      return `(${gen(e.left)} ${op} ${gen(e.right)})`;
     },
     UnaryExpression(e) {
       if (e.op === "some") {
-        e.op = ""
+        e.op = "";
       }
-      return `${e.op}(${gen(e.operand)})`
+      return `${e.op}(${gen(e.operand)})`;
     },
     SubscriptExpression(e) {
-      return `${gen(e.array)}[${gen(e.index)}]`
+      return `${gen(e.array)}[${gen(e.index)}]`;
     },
     ArrayExpression(e) {
-      console.log(e)
-      if (e?.type?.baseType instanceof SumType) return `{${gen(e.elements).map((e,i)=>{return `${i}: ${e}`}).join(",")}}`
-      return `[${gen(e.elements).join(",")}]`
+      if (e?.type?.baseType instanceof SumType)
+        return `{${gen(e.elements)
+          .map((e, i) => {
+            return `${i}: ${e}`;
+          })
+          .join(",")}}`;
+      return `[${gen(e.elements).join(",")}]`;
     },
     EmptyArray(e) {
-      return "[]"
+      return "[]";
     },
     MemberExpression(e) {
-      const object = gen(e.object)
-      const field = JSON.stringify(gen(e.field))
-      const chain = e.isOptional ? "?." : ""
-      return `(${object}${chain}[${field}])`
+      const object = gen(e.object);
+      const field = JSON.stringify(gen(e.field));
+      const chain = e.isOptional ? "?." : "";
+      return `(${object}${chain}[${field}])`;
     },
     OgreCall(c) {
       const targetCode = standardOgres.has(c.callee)
         ? standardOgres.get(c.callee)(gen(c.args))
-        : `${gen(c.callee)}(${gen(c.args).join(", ")})`
+        : `${gen(c.callee)}(${gen(c.args).join(", ")})`;
       // Calls in expressions vs in statements are handled differently
       if (c.callee.type.returnType !== Type.VOID) {
-        return targetCode
+        return targetCode;
       }
-      output.push(`${targetCode};`)
+      output.push(`${targetCode};`);
     },
     ConstructorCall(c) {
-      return `new ${gen(c.callee)}(${gen(c.args).join(", ")})`
+      return `new ${gen(c.callee)}(${gen(c.args).join(", ")})`;
     },
     Number(e) {
-      return e
+      return e;
     },
     BigInt(e) {
-      return e
+      return e;
     },
     Boolean(e) {
-      return e
+      return e;
     },
     String(e) {
-      return e
+      return e;
     },
     Array(a) {
-      return a.map(gen)
+      return a.map(gen);
     },
-  }
-  gen(program)
-  return output.join("\n")
+  };
+  gen(program);
+  return output.join("\n");
 }
